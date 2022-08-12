e3b8a763225e40f0d2f4c164a00d8ad869441767
javiervargas <javiervg1802@gmail.com>
2022-08-12 18:41:50 -0500

    role question function

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const { printTable } = require('console-table-printer');
-const { default: Choice } = require('inquirer/lib/objects/choice');
-const { default: Choices } = require('inquirer/lib/objects/choices');
+const cTable = require("console.table");
 
         //This is the information for sql database.
 const db = mysql.createConnection({
 
-    host: 'localhost',
+    host: 'Localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'Kromina123',
     database: 'employees_db'
 
     });
@@ -53,16 +51,73 @@ function start() {
     });
 }
 
-
+        //View Questions
 function view(){
+    inquirer
+    .prompt([{
+            type: "list",
+            name: "view",
+            message: "Select one option for see content",
+            choices: ["All employees", "Departments", "Roles"]
+        }
 
-
-
-
-
-
-
-
-
-
+    ]).then (function(res){
+        switch(res.view){
+            case "All Employees":
+                ViewAllEmployees(); 
+                break;
+            case "Departments":
+                ViewDepartemts();
+                break;
+            case "Roles":
+                ViewAllRoles();
+                break;
+            default: console.log("default");
+        }
+    });
 }
+
+        // Role Questions
+function role(){
+    db.query("Select one name from role", function(err, results){
+        if(err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function(){
+                        var choiceArr = [];
+                        for(i=0; i< results.length; i++){
+                            choiceArr.push(results[i].title);
+                        }
+                        return choiceArr;
+                    },
+                    message: "Select role"
+                }
+            ]).then(function(answer){
+                console.log(answer.choice);
+                db.query(
+                    "SELECT e.id AS ID, e.first_name AS First, e.last_name AS LAST, e.ROLE_id, r.salary AS Salary, M.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON  r.department_id = d.id WHERE e.role_id = ?", [answer.choice], function(err, results){
+                        if(err)  throw err;
+                        console.table(results);
+                        start();
+                    }
+                    
+                )
+            });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
